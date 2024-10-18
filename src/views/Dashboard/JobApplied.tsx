daed7307920e53457585429c6cff673f76d6265c
mahamzenkoders <mahamzenkoders@gmail.com>
2024-10-18 18:32:42 +0500

    resolved error

--- src/views/Dashboard/JobApplied.tsx
+++ src/views/Dashboard/JobApplied.tsx
@@ -1,16 +1,16 @@
-import JobApplied from '@/components/JobApplied'
-import SideNav from '@/components/SideNav'
-import React from 'react'
+import JobApplied from '@/components/JobApplied';
+import SideNav from '@/components/SideNav';
+import React from 'react';
 
 const JobAppliedView = () => {
   return (
     <div className='flex'>
       <div className='lg:block hidden'>
-        <SideNav />
+        <SideNav role='jobSeeker' />
       </div>
       <JobApplied />
     </div>
-  )
-}
+  );
+};
 
-export default JobAppliedView
+export default JobAppliedView;
